44b30b3edc7f67adfaa6a25d6b9d8893239f188c
NedimKunovac <kunovac.nedim@outlook.com>
2024-06-15 16:34:16 +0200

    fixed button placement

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
   <h1 class="text-3xl font-bold underline">BooksAPI</h1>
-  <button
-    type="button"
-    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
-  >
-    <svg
-      class="w-5 h-5"
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 18 18"
-    >
-      <path
-        d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z"
-      />
-    </svg>
-  </button>
-  <!--
-  <ul class="grid grid-cols-3">
-      <li v-for="book in books" :key="book.id"><img :src="book.cover_image" class="h-24 w-24"></img>{{ book.title }} by {{ book.author }} {{ book.publication_year }}</li>
-  </ul>
-  -->
   <ul role="list" class="divide-y divide-gray-100 grid grid-cols-3 mx-20">
     <li
       v-for="book in books"
@@ -46,9 +25,22 @@
         <p class="text-sm leading-6 text-gray-900">
           {{ book.publication_year }}
         </p>
-        <p class="mt-1 text-xs leading-5 text-gray-500">
-          Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
-        </p>
+        <button
+          type="button"
+          class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
+        >
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 18 18"
+          >
+            <path
+              d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z"
+            />
+          </svg>
+        </button>
       </div>
     </li>
   </ul>
