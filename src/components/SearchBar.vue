741642ec489f13f2d183c2a4920195df98a23f99
NedimKunovac <kunovac.nedim@outlook.com>
2024-06-19 15:49:31 +0200

    Implemented comments and saved their state Modified some UI elements to be responsive

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,7 +7,7 @@
       <input
         type="text"
         v-model="localSearchQuery"
-        class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm leading-6"
         placeholder="Enter book title here"
         @input="$emit('update:searchQuery', localSearchQuery)"
       />
